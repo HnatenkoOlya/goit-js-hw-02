26fa5105715159917c9b73f5d6083589030512ab
HnatenkoOlya <gnatenkoolgaig1210@gmail.com>
2024-12-05 20:09:51 +0100

    hw2

---
+++ js/task-4.js
@@ -0,0 +1,27 @@
+function getShippingCost(country) {
+    let cost;
+    const defaultMessage = "Sorry, there is no delivery to your country";
+    switch (country) {
+        case "China":
+        cost = 100;
+        return `Shipping to ${country} will cost ${cost} credits`
+        case "Chile":
+        cost = 250;
+        return `Shipping to ${country} will cost ${cost} credits`
+        case "Australia":
+        cost = 170;
+        return `Shipping to ${country} will cost ${cost} credits`
+        case "Jamaica":
+        cost = 120;
+        return `Shipping to ${country} will cost ${cost} credits`
+        default: return defaultMessage;
+
+    }
+}
+console.log(getShippingCost("Australia")); // "Shipping to Australia will cost 170 credits"
+console.log(getShippingCost("Germany")); // "Sorry, there is no delivery to your country"
+console.log(getShippingCost("China")); // "Shipping to China will cost 100 credits"
+console.log(getShippingCost("Chile")); // "Shipping to Chile will cost 250 credits"
+console.log(getShippingCost("Jamaica")); // "Shipping to Jamaica will cost 120 credits"
+console.log(getShippingCost("Sweden")); // "Sorry, there is no delivery to your country"
+
